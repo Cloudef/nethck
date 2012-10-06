49b90c874b8b74cb52c291a83770b7f6b49b430c
Jari Vetoniemi <mailroxas@gmail.com>
2012-10-06 18:26:33 +0300

    init

---
+++ example/src/server.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <GL/glfw3.h>
+#include <glhck/glhck.h>
+#include <glhck/nethck.h>
+
+int main(int argc, char **argv)
+{
+   nethckInit(argc, argv);
+   return EXIT_SUCCESS;
+}
